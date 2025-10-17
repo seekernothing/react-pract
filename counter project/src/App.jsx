22f60246db112309652bdaa5af0b306b54eb19e4
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-10-17 16:15:11 +0530

    counter project

---
+++ counter project/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+
+const Counter = ()=>{
+
+   const style = { textAlign: "center",marginTop:"10px" };
+
+  const [count,setCount] = useState(0)
+
+  //console.log(count)
+
+  return(
+    <div style={style}>
+
+      <h3>Counter app</h3>
+
+      <h1>{count}</h1>
+
+      <button 
+      style={{backgroundColor:"black",color:"White",cursor:"pointer",padding:"2px 2px"}}
+      onClick={()=>setCount(count+1)}>Increment</button>
+
+
+    </div>
+  )
+
+}
+
+export default Counter
