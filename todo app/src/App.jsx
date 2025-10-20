151ef1ed01cb48d78919edc1dcb447c707a893be
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-10-20 23:51:37 +0530

    initial commit

---
+++ todo app/src/App.jsx
@@ -0,0 +1,31 @@
+
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Todo App</h1>
+
+
+      <div>
+
+        <input
+        type="text"
+        placeholder="Enter your task"
+        />
+
+        <button>Add</button>
+      </div>
+
+      <div>
+        <ul>
+          <li>Run of 1km</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
