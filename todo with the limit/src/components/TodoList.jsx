c8ba20ac1a9843d3db9909abaca01e06e3ee30d1
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-10-30 23:18:26 +0530

    added limit functionality

--- todo with the limit/src/components/TodoList.jsx
+++ todo with the limit/src/components/TodoList.jsx
@@ -8,6 +8,30 @@ const TodoList = () => {
 
     const[input,setInput] = useState("")
    
+    const[list,setList] = useState([])
+
+   let limit = 5
+
+   
+
+function handelAdd(){
+
+
+     if (list.length >= limit) {
+       Error(`You can add at most ${limit} todos.`);
+     }
+
+
+   if (input.trim() !== "") {
+     setList([...list, input]);
+   }
+   setInput("");
+}
+
+function handelDelete(i){
+    let newList = list.filter((l,idx)=>idx!==i)
+    setList(newList)
+}
 
 
 
@@ -20,18 +44,20 @@ const TodoList = () => {
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
+
+        <button 
+        disabled={list.length>=limit}
+        onClick={handelAdd}>Add</button>
       </div>
 
-      <div className='product display'>
-
-
-        <button onClick={handelAdd}>Add</button>
-
-
-<ul>
-
-</ul>
-
+      <div className="product display">
+        <ul className='ul'>
+          {list.map((l, i) => (
+            <li key={i}> {l}
+            <button onClick={()=>handelDelete(i)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
