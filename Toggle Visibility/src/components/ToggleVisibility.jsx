902b09f738e1e35f307fc77c314821625a51060c
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-11-01 03:15:41 +0530

    Implement product visibility toggle and add product data

--- Toggle Visibility/src/components/ToggleVisibility.jsx
+++ Toggle Visibility/src/components/ToggleVisibility.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import product from '../utils/prodcuts.json'
+import { useState } from 'react'
 
 const ToggleVisibility = () => {
+
+    const[products,setProducts] = useState(product)
+    const visibleProducts = products.filter((p)=>p.visible===true)
+    console.log(visibleProducts)
+
+    const handelToggle = (pid)=>{
+        let toggle = 
+    }
+
   return (
-    <div>ToggleVisibility</div>
-  )
+    <div>
+        <h2>Prodcts</h2>
+      <ul className='li'>
+
+        {visibleProducts.map((p)=>(
+            <li key={p.id}>{p.name}
+            <button onClick={()=>handelToggle(p.id)}>ToggleVisibility</button>
+            </li>
+        ))}
+       
+      </ul>
+    </div>
+  );
 }
 
 export default ToggleVisibility
