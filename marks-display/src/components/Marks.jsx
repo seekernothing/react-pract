f6156e3035d99045ecfe8f5991b5e53e241afad7
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-11-01 15:49:35 +0530

    intial solution

--- marks-display/src/components/Marks.jsx
+++ marks-display/src/components/Marks.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import list from "../utils/list.json"
+
 const Marks = () => {
   return (
-    <div>Marks</div>
+    <div>
+        <h2><b>Student List</b></h2>
+ 
+ <ul>
+    {list.map((l,idx)=>(
+        <li key={idx}>{`${l.name}--${l.marks}`}</li>
+    ))}
+ </ul>
+
+    </div>
   )
 }
 
