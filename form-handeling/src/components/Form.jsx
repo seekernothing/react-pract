8e8be0c384f9311c695de3324a2b85e372710832
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-11-01 16:56:46 +0530

    Implement feedback form with state management and display submitted data

--- form-handeling/src/components/Form.jsx
+++ form-handeling/src/components/Form.jsx
@@ -1,23 +1,82 @@
 import { useState } from "react";
 
 const Form = () => {
+
+    const[name,setName] = useState("")
+    const[email,setEmail] = useState("")
+    const[message,setMessage] = useState("")
+    const[submittedData,setSubmitteddata] = useState(null)
+
+    console.log(submittedData);
+    
+
+    const handelOnsubmit= (e)=>{
+          e.preventDefault()
+          setSubmitteddata({name,email,message})
+          setName("")
+          setEmail("")
+          setMessage("")
+    }
   return (
     <div>
       <h2>
         <b>Feedback Form</b>
       </h2>
 
-      <form>
-        <input type="text" placeholder="Name" required />
+      <form onSubmit={handelOnsubmit}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+          required
+        />
         <br></br>
         <br></br>
-        <input type="email" placeholder="Email" required />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+          required
+        />
         <br></br> <br></br>
-        <textarea placeholder="Message"></textarea>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message"
+        ></textarea>
         <br></br> <br></br>
         <button type="submit">Submit</button>
       </form>
+
+
+
+            {
+                submittedData && 
+               <div style={{marginTop:"10px"}}>
+
+                <h2><b>Submitted Data</b></h2>
+                <p>
+                    <b>Name : {submittedData.name}</b>
+                </p>
+                <p>
+                    <b>Email : {submittedData.email}</b>
+                </p>
+                <p>
+                    <b>Message: {submittedData.message}</b>
+                </p>
+                
+                  </div>
+            }
+
+
+
     </div>
+
+
+
+
   );
 };
 
